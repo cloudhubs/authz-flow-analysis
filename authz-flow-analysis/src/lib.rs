11b7858e026476e38d63e3c3331fbbe4cdffd9f6
nnazo <jacobcurtis2266@gmail.com>
2022-04-22 18:10:23 -0500

    fix compile error

--- authz-flow-analysis/src/lib.rs
+++ authz-flow-analysis/src/lib.rs
@@ -121,8 +121,8 @@ impl BitOr for EndpointCrudAccess {
     fn bitor(self, rhs: Self) -> Self::Output {
         let mut merged = self.0.clone();
 
-        for (entity, crud_op) in self.0 {
-            let other_crud_op = merged.entry(entity).or_insert(crud_op);
+        for (entity, crud_op) in self.0.into_iter().chain(rhs.0.into_iter()) {
+            let other_crud_op = merged.entry(entity.clone()).or_insert(crud_op);
             *other_crud_op |= crud_op;
             merged.insert(entity, crud_op);
         }
@@ -133,7 +133,7 @@ impl BitOr for EndpointCrudAccess {
 
 impl BitOrAssign for EndpointCrudAccess {
     fn bitor_assign(&mut self, rhs: Self) {
-        *self = *self | rhs;
+        *self = self.clone() | rhs.clone();
     }
 }
 
@@ -185,64 +185,22 @@ pub fn infer_crud_flows(
         }
     }
 
-    let services_aux = services.clone();
+    let mut services_aux = services.clone();
 
-    for service in services.services.iter_mut() {
-        for endpoint in service.endpoints.iter_mut() {
-            // Go through the calls each endpoint makes to expand permissions
-            let calls: Vec<_> = service
-                .calls
-                .iter()
-                .filter(|call| call.from.contains(&endpoint.name))
-                .collect();
-
-            for call in calls {
-                let service = match services_aux
-                    .services
-                    .iter()
-                    .find(|s| service.name == call.service)
-                {
-                    Some(s) => s,
-                    _ => continue,
-                };
-
-                let called_endpoint = service
-                    .endpoints
-                    .iter()
-                    .find(|e| e.name == call.endpoint)
-                    .expect("Missing endpoint");
-
-                match endpoint.flow_crud_access.as_mut() {
-                    Some(flow_crud_access) => {
-                        // *flow_crud_access |= called_endpoint.flow_crud_access;
-                    }
-                    _ => endpoint.flow_crud_access = called_endpoint.flow_crud_access.clone(),
-                }
-            }
+    let mut visited = HashMap::new();
+    for (i, service) in services.services.iter().enumerate() {
+        for (j, endpoint) in service.endpoints.iter().enumerate() {
+            let new_endpoint =
+                visit_crud_flows(&services.services, &mut visited, service, endpoint);
+            let service_aux = services_aux.services.get_mut(i).unwrap();
+            let endpoint_aux = service_aux.endpoints.get_mut(j).unwrap();
+            *endpoint_aux = new_endpoint;
         }
     }
-    services
+
+    services_aux
 }
 
-/* services Service[]
- * visited HashMap<Endpoint, ()>
- *
- * service Service
- * endpoint Endpoint
- *
- * calls = service.calls.filter(|call| call.from.find(endpoint.path))
- * visited.insert(endpoint)
- *
- * for call in calls {
- *   called_svc = services.find(service.name)
- *   called_endpoint = called_svc.endpoints.find(call.path)
- *   if visited.get(called_endpoint).is_none() {
- *     // compute permissions
- *     recurse(services, called_svc, called_endpoint, visited)
- *   }
- *   endpoint.flow_crud_access |= called_endpoint.flow_crud_access
- * }
- */
 fn visit_crud_flows(
     services: &Vec<Service>,
     visited: &mut HashMap<String, ()>, // ServiceEndpoint.name
@@ -272,13 +230,26 @@ fn visit_crud_flows(
             _ => continue,
         };
 
+        // Recursively populate endpoint CRUD flows
         let called_endpoint = if !visited.contains_key(&called_endpoint.name) {
             visit_crud_flows(services, visited, called_svc, called_endpoint)
         } else {
             called_endpoint.clone()
         };
 
-        new_endpoint.flow_crud_access |= called_endpoint.flow_crud_access;
+        // Merge the endpoint flows
+        match (
+            new_endpoint.flow_crud_access.as_mut(),
+            called_endpoint.flow_crud_access.clone(),
+        ) {
+            (Some(new_flow_crud_access), Some(flow_crud_access)) => {
+                *new_flow_crud_access |= flow_crud_access;
+            }
+            (None, Some(flow_crud_access)) => {
+                new_endpoint.flow_crud_access = Some(flow_crud_access);
+            }
+            _ => {}
+        }
     }
     new_endpoint
 }
